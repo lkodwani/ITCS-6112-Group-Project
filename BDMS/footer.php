d1551adf286cbff23a5d7db7997a81f6c1bac16f
lkodwani <130891531+lkodwani@users.noreply.github.com>
2023-12-04 20:19:13 -0500

    Add files via upload

---
+++ BDMS/footer.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>
+  Blood Bank Management System
+  <br>
+  Implemented by: Group 1(Blood Buddies).
+  </center>
+  </div>
+
+</body>
+
+</html>
